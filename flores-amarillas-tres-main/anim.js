af5e65def19d88dada310392daad980665323bf4
SuzuKaz3 <FerJVizcarra@gmail.com>
2025-09-22 01:16:12 -0500

    Add files via upload

---
+++ flores-amarillas-tres-main/anim.js
@@ -0,0 +1,54 @@
+// Sincronizar las letras con la canción
+var audio = document.querySelector("audio");
+var lyrics = document.querySelector("#lyrics");
+
+// Array de objetos que contiene cada línea y su tiempo de aparición en segundos
+var lyricsData = [
+  { text: "You where my strength when i was weak 🍰", time: 15 },
+  { text: "Your are my voice when i couldn't speak 🍓", time: 18 },
+  { text: "You were my eyes when i couldn't see 🌙", time: 21 },
+  { text: "You saw the best there was in me 🌸", time: 24 },
+  { text: "Lifted me up when i couldn't reach 🧁", time: 27 },
+  { text: "You gave me faith' cause you believed 🎀", time: 30 },
+  { text: "I'm everything I am because you loved me 💍", time: 33 },
+  { text: "TE AMO ⛄", time: 33 },
+
+];
+
+// Animar las letras
+function updateLyrics() {
+  var time = Math.floor(audio.currentTime);
+  var currentLine = lyricsData.find(
+    (line) => time >= line.time && time < line.time + 6
+  );
+
+  if (currentLine) {
+    // Calcula la opacidad basada en el tiempo en la línea actual
+    var fadeInDuration = 0.1; // Duración del efecto de aparición en segundos
+    var opacity = Math.min(1, (time - currentLine.time) / fadeInDuration);
+
+    // Aplica el efecto de aparición
+    lyrics.style.opacity = opacity;
+    lyrics.innerHTML = currentLine.text;
+  } else {
+    // Restablece la opacidad y el contenido si no hay una línea actual
+    lyrics.style.opacity = 0;
+    lyrics.innerHTML = "";
+  }
+}
+
+setInterval(updateLyrics, 1000);
+
+//funcion titulo
+// Función para ocultar el título después de 216 segundos
+function ocultarTitulo() {
+  var titulo = document.querySelector(".titulo");
+  titulo.style.animation =
+    "fadeOut 3s ease-in-out forwards"; /* Duración y función de temporización de la desaparición */
+  setTimeout(function () {
+    titulo.style.display = "none";
+  }, 3000); // Espera 3 segundos antes de ocultar completamente
+}
+
+// Llama a la función después de 216 segundos (216,000 milisegundos)
+setTimeout(ocultarTitulo, 216000);
